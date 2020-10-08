85b7ac623a9ab4369c8387a932819b785d730443
Wei Ding <ding_web@yahoo.co.jp>
2020-10-07 23:38:22 -0400

    two methods

--- Collection/NoRepeatingCharacters/Characters.cpp
+++ Collection/NoRepeatingCharacters/Characters.cpp
@@ -6,14 +6,28 @@ void Characters::noRepeating() {
 	std::cout << "== Length of a string without repeating characters\n";
 
 	lengthOfLongestSubstring("abcdefgabcdefh");
+	lengthOfLongestSubstring("123333333333333333331");
+	lengthOfLongestSubstring("hello everybody!");
 }
 
 void Characters::lengthOfLongestSubstring(std::string s) {
+	std::cout << "[" << s << "]";
+	useInsert(s);
+	useDefine(s);
+	std::cout << "\n";
+}
+
+void Characters::useInsert(std::string s) {
 	std::unordered_set<char> chars;
 
 	for (int i = 0; i < s.size(); i++)
 		if (chars.find(s[i]) == chars.end())
 			chars.insert(s[i]);
-	
-	std::cout << "<" << s << ">" << ", length <" << chars.size() << ">\n";
+
+	std::cout << " By Inserting: <" << chars.size() << ">.";
+}
+
+void Characters::useDefine(std::string s) {
+	std::unordered_set<char> chars(s.begin(), s.end());
+	std::cout << " By Defining: <" << chars.size() << ">.";
 }
--- Collection/NoRepeatingCharacters/Characters.h
+++ Collection/NoRepeatingCharacters/Characters.h
@@ -7,5 +7,7 @@ public:
 	void noRepeating();
 private:
 	void lengthOfLongestSubstring(std::string);
+	void useInsert(std::string);
+	void useDefine(std::string);
 };
 
