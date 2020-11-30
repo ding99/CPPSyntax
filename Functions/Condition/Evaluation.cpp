882c06d0821a4683c5164b16c783b9cba7dd8e81
Wei Ding <ding_web@yahoo.co.jp>
2020-11-29 20:21:23 -0500

    switch expression must be integer or enum

--- Functions/Condition/Evaluation.cpp
+++ Functions/Condition/Evaluation.cpp
@@ -1,5 +1,6 @@
 #include "Evaluation.h"
 #include <iostream>
+#include <string>
 
 void Evaluation::Start() {
 	std::cout << "-- Examining Switch" << std::endl;
@@ -16,16 +17,4 @@ void Evaluation::Start() {
 		default: std::cout << "Sunday"; break;
 	}
 	std::cout << std::endl;
-
-	std::cout << "The expression is a string variable" << std::endl;
-	day = 3;
-	switch (day) {
-	case 1: std::cout << "Monday"; break;
-	case 2: std::cout << "Tuesday"; break;
-	case 3: std::cout << "Wednesday"; break;
-	case 4: std::cout << "Thursday"; break;
-	case 5: std::cout << "Friday"; break;
-	case 6: std::cout << "Saturday"; break;
-	default: std::cout << "Sunday"; break;
-	}
 }
